369db4a89322d01dd045ebefbeae807c5f83d192
Rem Kim <kim.rmi.93@gmail.com>
2019-11-27 21:56:39 -0500

    material-ui added

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,15 @@
 import App from "next/app";
 import React from "react";
-import { ThemeProvider } from "styled-components";
-
-const theme = {
-  colors: {
-    primary: "#0070f3"
-  }
-};
+import { ThemeProvider } from "@material-ui/styles";
+import theme from "../src/theme";
+import { CssBaseline } from "@material-ui/core";
 
 export default class AdsoApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
       <ThemeProvider theme={theme}>
+        <CssBaseline />
         <Component {...pageProps} />
       </ThemeProvider>
     );
