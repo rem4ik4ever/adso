f862fb681ecc7dbc7882f1446bbd7ded92d976aa
Rem Kim <kim.rmi.93@gmail.com>
2019-12-21 15:26:22 -0500

    post id route

--- pages/p/[id]/index.js
+++ pages/p/[id]/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useRouter } from "next/dist/client/router";
 import Head from "next/head";
-import PostView from "../src/components/Post/PostView";
+import PostView from "../../../src/components/Post/PostView";
 const Post = () => {
   const router = useRouter();
   const { id } = router.query;
